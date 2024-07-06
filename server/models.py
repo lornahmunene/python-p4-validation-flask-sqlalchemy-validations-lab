8fb2e5786c2594c985e679c196e1a94d43941c70
LornahMunene <munenelornah9@gmail.com>
2024-07-06 11:56:04 +0300

    validations lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,20 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('phone_number')
+    def validates_phone_number(self,key,digits):
+        if len(digits) !=10 or not digits.isdigit() :
+         raise ValueError('Phone_number should be less than 10 digits and numeric')
+        return digits
+    @validates ('name')
+    def validates_name(self,key,name):
+        if not name :
+            raise ValueError("Author name is required")
+        if Author.query.filter_by(name=name).first():
+            raise ValueError(f"Author with name '{name}' already exists.")
+        return name
+    
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +41,18 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('content')
+    def validate_content(self,key,content):
+        if len(content)!=250:
+            raise ValueError("Content is too short")
+        return content
+    @validates('summary')
+    def validate_summary(self,key,summary):
+        if len(summary)>250:
+            raise ValueError("Summary is too long")
+        return summary
+
 
 
     def __repr__(self):
